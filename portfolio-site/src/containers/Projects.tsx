22f2972464ed09f0915b0987c684ec2a030722c8
Riel St. Amand <riel.stamand@gmail.com>
2021-11-01 14:09:00 -0400

    Projects container backbone

---
+++ portfolio-site/src/containers/Projects.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import FadeInSection from "../components/Animations/FadeInSection";
+import Container from "@material-ui/core/Container";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+
+const useStyles = makeStyles((theme: Theme) => createStyles({}));
+
+const Projects: React.FC = () => {
+    const classes = useStyles();
+    return (
+        <section id="section4">
+            <FadeInSection>
+                <Container maxWidth="md">
+                    <Box textAlign="center"></Box>
+                </Container>
+            </FadeInSection>
+        </section>
+    );
+};
+
+export default Projects;
