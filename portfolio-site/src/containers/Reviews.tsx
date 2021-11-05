59e258278496d42dd9f97eab464a45562030703c
Riel St. Amand <riel.stamand@gmail.com>
2021-11-05 15:50:19 -0400

    Reviews container backbone

---
+++ portfolio-site/src/containers/Reviews.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import FadeInSection from "../components/Animations/FadeInSection";
+import Container from "@material-ui/core/Container";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import "./main.css";
+
+const useStyles = makeStyles((theme: Theme) => createStyles({}));
+
+const Reviews: React.FC = () => {
+    const classes = useStyles();
+    return (
+        <section id="section4">
+            <FadeInSection>
+                <Container maxWidth="md">
+                    <Box textAlign="center"></Box>
+                </Container>
+            </FadeInSection>
+        </section>
+    );
+};
+
+export default Reviews;
