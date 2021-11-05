8a07528ecbcaaebb6cd34d65948ad1fb936dac42
Riel St. Amand <riel.stamand@gmail.com>
2021-11-05 15:49:43 -0400

    Turn nav tabs into unique button component

---
+++ portfolio-site/src/components/Navigation/NavButton.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { spotifyBlack, NavButtonData } from "../../types";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { Link } from "react-scroll";
+import "../containers/main.css";
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        text: {
+            color: spotifyBlack,
+            fontSize: 16,
+            fontFamily: "Gotham-Black",
+        },
+    })
+);
+
+const NavButton: React.FC<NavButtonData> = ({ label, sectionId }) => {
+    const classes = useStyles();
+    return (
+        <Link
+            to={sectionId}
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            duration={1000}
+        >
+            <Button>
+                <Typography variant="h6" className={classes.text}>
+                    {label}
+                </Typography>
+            </Button>
+        </Link>
+    );
+};
+
+export default NavButton;
