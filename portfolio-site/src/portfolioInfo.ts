1d765c57e3841e015bfae1d3ec15c91561e18ce4
Riel St. Amand <riel.stamand@gmail.com>
2021-11-08 21:00:09 -0500

    add github urls to portfolio info

--- portfolio-site/src/portfolioInfo.ts
+++ portfolio-site/src/portfolioInfo.ts
@@ -25,6 +25,8 @@ export const name: string = "Riel St. Amand";
 export const intro: string =
     "A Machine Learning Engineer and Full Stack Developer. As a self-taught developer, my goal is always the same with any project: Learn, Improve, and Produce.";
 export const linkedInUrl: string = "https://www.linkedin.com/in/riel-st-amand/";
+export const gitHubUrl: string = "https://github.com/CanadaApollo6";
+export const gitHubUrlWork: string = "https://github.com/RielStAmandSD";
 
 export const navTabs: NavButtonData[] = [
     {
