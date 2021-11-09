75f85f67781ee61d3f0c504589e7fc3201f8b7aa
Riel St. Amand <riel.stamand@gmail.com>
2021-11-08 21:01:44 -0500

    get skills data from portfolio info

--- portfolio-site/src/containers/Skills.tsx
+++ portfolio-site/src/containers/Skills.tsx
@@ -6,24 +6,14 @@ import Grid from "@material-ui/core/Grid";
 import SkillAvatar from "../components/SkillAvatar";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import { SkillAvatarProps, spotifyGreen } from "../types";
-import PythonLogo from "../images/python-logo.png";
-import TensorFlowLogo from "../images/tensorflow-logo.png";
-import TypeScriptLogo from "../images/typescript-logo.png";
-import SqlLogo from "../images/sql-logo.png";
-import ReactLogo from "../images/react-logo.png";
 import Animation from "../components/Animations/Animation";
 import WebDevAnimation from "../components/Animations/JSON/web-developer.json";
-import JsLogo from "../images/js-logo.png";
-import HtmlLogo from "../images/html-logo.png";
-import CssLogo from "../images/css-logo.png";
-import RLogo from "../images/r-logo.png";
-import OfficeLogo from "../images/office-logo.png";
-import FlutterLogo from "../images/flutter-logo.png";
 import Button from "@material-ui/core/Button";
 import ArrowRightAlt from "@material-ui/icons/ArrowRightAlt";
 import { Link } from "react-scroll";
 import "./main.css";
 import FadeInSection from "../components/Animations/FadeInSection";
+import { skills } from "../portfolioInfo";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -43,52 +33,6 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const Skills: React.FC = () => {
-    const skillList: SkillAvatarProps[] = [
-        {
-            skillName: "Python",
-            imageSrc: PythonLogo,
-        },
-        {
-            skillName: "TensorFlow 2.0+",
-            imageSrc: TensorFlowLogo,
-        },
-        {
-            skillName: "TypeScript",
-            imageSrc: TypeScriptLogo,
-        },
-        {
-            skillName: "SQL & SSMS",
-            imageSrc: SqlLogo,
-        },
-        {
-            skillName: "React",
-            imageSrc: ReactLogo,
-        },
-        {
-            skillName: "JavaScript (ES6+)",
-            imageSrc: JsLogo,
-        },
-        {
-            skillName: "HTML5",
-            imageSrc: HtmlLogo,
-        },
-        {
-            skillName: "CSS3",
-            imageSrc: CssLogo,
-        },
-        {
-            skillName: "R",
-            imageSrc: RLogo,
-        },
-        {
-            skillName: "Office 365",
-            imageSrc: OfficeLogo,
-        },
-        {
-            skillName: "Flutter",
-            imageSrc: FlutterLogo,
-        },
-    ];
     const classes = useStyles();
     return (
         <section id="section3">
@@ -115,7 +59,7 @@ const Skills: React.FC = () => {
                             spacing={5}
                             className={classes.container}
                         >
-                            {skillList.map((skill: SkillAvatarProps) => (
+                            {skills.map((skill: SkillAvatarProps) => (
                                 <Grid item xs={6} md={2}>
                                     <SkillAvatar
                                         imageSrc={skill.imageSrc}
