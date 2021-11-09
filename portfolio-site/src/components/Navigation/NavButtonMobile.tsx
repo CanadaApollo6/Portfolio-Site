22542937779e2c6aba2b295cf1cd16e1a077d6de
Riel St. Amand <riel.stamand@gmail.com>
2021-11-08 20:55:08 -0500

    new nav for mobile devices

---
+++ portfolio-site/src/components/Navigation/NavButtonMobile.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { NavButtonData, spotifyGreen } from "../../types";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import { Link } from "react-scroll";
+import "../../containers/main.css";
+
+const useStyles = makeStyles(() =>
+    createStyles({
+        text: {
+            color: spotifyGreen,
+            fontFamily: "Gotham-Black",
+        },
+    })
+);
+
+interface NavButtonMobileProps extends NavButtonData {
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const NavButtonMobile: React.FC<NavButtonMobileProps> = ({
+    label,
+    sectionId,
+    setOpen,
+}) => {
+    const classes = useStyles();
+    return (
+        <Link
+            to={sectionId}
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            duration={2500}
+            onClick={() => setOpen(false)}
+        >
+            <Button>
+                <Typography variant="h6" className={classes.text}>
+                    {label}
+                </Typography>
+            </Button>
+        </Link>
+    );
+};
+
+export default NavButtonMobile;
--- portfolio-site/src/components/Navigation/NavMobile.tsx
+++ portfolio-site/src/components/Navigation/NavMobile.tsx
@@ -1,1 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Drawer from "@material-ui/core/Drawer";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Typography from "@material-ui/core/Typography";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import "../../containers/main.css";
+import { NavButtonData, spotifyBlack, spotifyGreen } from "../../types";
+import { name, linkedInUrl, navTabs } from "../../portfolioInfo";
+import NavButtonMobile from "./NavButtonMobile";
+import Divider from "@material-ui/core/Divider";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        navBar: {
+            backgroundColor: spotifyGreen,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        name: {
+            fontFamily: "Agustina",
+            color: spotifyBlack,
+            flexGrow: 1,
+        },
+        linkedIn: {
+            marginRight: theme.spacing(1),
+        },
+        topOfMenu: {
+            color: spotifyBlack,
+            fontFamily: "Gotham-Black",
+        },
+    })
+);
+
+const NavMobile: React.FC = () => {
+    const classes = useStyles();
+    const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
+    return (
+        <>
+            <AppBar position="fixed" className={classes.navBar}>
+                <ToolBar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        onClick={() => setDrawerOpen(true)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography className={classes.name} variant="h6">
+                        {name}
+                    </Typography>
+                    <a href={linkedInUrl}>
+                        <IconButton edge="end" className={classes.linkedIn}>
+                            <LinkedInIcon />
+                        </IconButton>
+                    </a>
+                </ToolBar>
+            </AppBar>
+            <Drawer
+                anchor="left"
+                open={drawerOpen}
+                onClose={() => setDrawerOpen(false)}
+            >
+                <List>
+                    <ListItem>
+                        <Typography variant="h5" className={classes.topOfMenu}>
+                            SECTIONS
+                        </Typography>
+                    </ListItem>
+                    <Divider />
+                    {navTabs.map((buttonInfo: NavButtonData) => (
+                        <ListItem>
+                            <NavButtonMobile
+                                label={buttonInfo.label}
+                                sectionId={buttonInfo.sectionId}
+                                setOpen={setDrawerOpen}
+                            />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+        </>
+    );
+};
+
+export default NavMobile;
