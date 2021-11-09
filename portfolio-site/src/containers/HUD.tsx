758596b3c1e95e856a1aa8fad52c26ece1bbf145
Riel St. Amand <riel.stamand@gmail.com>
2021-11-08 21:02:40 -0500

    updated HUD with new nav and containers

--- portfolio-site/src/containers/HUD.tsx
+++ portfolio-site/src/containers/HUD.tsx
@@ -5,6 +5,9 @@ import Skills from "./Skills";
 import NavTabsDesktop from "../components/Navigation/NavTabsDesktop";
 import "./main.css";
 import Hidden from "@material-ui/core/Hidden";
+import Projects from "./Projects";
+import Reviews from "./Reviews";
+import NavMobile from "../components/Navigation/NavMobile";
 
 const HUD: React.FC = () => {
     return (
@@ -17,9 +20,14 @@ const HUD: React.FC = () => {
             <Hidden smDown>
                 <NavTabsDesktop />
             </Hidden>
+            <Hidden mdUp>
+                <NavMobile />
+            </Hidden>
             <Head />
             <About />
             <Skills />
+            <Projects />
+            <Reviews />
         </div>
     );
 };
