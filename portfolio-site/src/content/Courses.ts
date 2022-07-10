0d14cc8529892ec53da8f07fb6fcafb1f59a77f5
Riel St. Amand <riel.stamand@gmail.com>
2022-07-10 02:14:57 -0400

    add new learning content

---
+++ portfolio-site/src/content/Courses.ts
@@ -0,0 +1,94 @@
+import { CourseCardData } from "../types";
+
+export const courses: CourseCardData[] = [
+  {
+    title: "Complete Web Developer in 2020: Zero To Mastery",
+    technologies: "HTML, CSS, JS, React, NodeJS, Git, GitHub",
+    description:
+      "An extensive course on the fundamentals of modern web development.",
+    hours: 35,
+    instructor: "Andrei Neagoie",
+    courseUrl: "https://academy.zerotomastery.io/courses/697434",
+    logo: "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/AF5HfT7YSZ6L7Eo5I1Wc",
+    courseCertificateUrl:
+      "https://drive.google.com/file/d/1Jbe8L6z-am5JKbg7r4qr3xNUrQDE_VOR/view?usp=sharing",
+    isComplete: true,
+  },
+  {
+    title: "Complete Python Developer in 2020: Zero To Mastery",
+    technologies:
+      "Python 3.X, Anaconda, Selenium, Jupyter Notebooks, SK-Learn, TensorFlow 2.X",
+    description:
+      "A large course teaching Python 3 from scratch and common uses of Python today.",
+    hours: 31,
+    instructor: "Andrei Neagoie",
+    courseUrl: "https://academy.zerotomastery.io/courses/694968",
+    logo: "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/9aMVrtQHRQWY2CxAxvyh",
+    courseCertificateUrl:
+      "https://drive.google.com/file/d/1NbGUTIqz1B4Xr3OlL9gG5sTsDTruTpl9/view?usp=sharing",
+    isComplete: true,
+  },
+  {
+    title: "JavaScript: The Advanced Concepts",
+    technologies: "ES6+ JavaScript",
+    description:
+      "A course focused on advancing a pre-existing base knowledge of JavaScript to a high level.",
+    hours: 25,
+    instructor: "Andrei Neagoie",
+    courseUrl: "https://academy.zerotomastery.io/courses/698487",
+    logo: "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/EusdpfMnQRy8JSDJU3cX",
+    courseCertificateUrl:
+      "https://drive.google.com/file/d/1_RGpjG7HXloKyE6XWNcvPDNmEQm46fjj/view?usp=sharing",
+    isComplete: true,
+  },
+  {
+    title: "Complete Machine Learning & Data Science Bootcamp 2022",
+    technologies:
+      "Python 3, SK-Learn, TensorFlow 2.X, Colab, Pandas, NumPy, MatPlotLib",
+    description:
+      "Mostly completed in summer 2020. It gave me a strong base in Python-driven ML.",
+    hours: 44,
+    instructor: "Daniel Bourke, Andrei Neagoie",
+    courseUrl: "https://academy.zerotomastery.io/courses/700485",
+    logo: "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/RDnD98pSzyAGUyKWmiyA",
+    courseCertificateUrl:
+      "https://drive.google.com/file/d/1njRd_G3Kah8l6XyLUF_GjtTEvDw4oNlE/view?usp=sharing",
+    isComplete: true,
+  },
+  {
+    title: "Master the Coding Interview: Data Structures + Algorithms",
+    technologies: "DS&A, JS, Python",
+    description:
+      "An ongoing course to help me gain a strong computer science foundation.",
+    hours: 20,
+    instructor: "Andrei Neagoie",
+    courseUrl: "https://academy.zerotomastery.io/courses/700474",
+    logo: "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/wtUQJ7aTDWvilZ83Wseh",
+    courseCertificateUrl: "",
+    isComplete: false,
+  },
+  {
+    title: "TensorFlow Developer Certificate in 2022",
+    technologies: "Python, Colab, TensorFlow 2.X",
+    description:
+      "An ongoing course to help me prepare for the TensorFlow Developer Exam.",
+    hours: 64,
+    instructor: "Daniel Bourke, Andrei Neagoie",
+    courseUrl: "https://academy.zerotomastery.io/courses/1240775",
+    logo: "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/OTA6qd3uRUiL8scd9Znh",
+    courseCertificateUrl: "",
+    isComplete: false,
+  },
+  {
+    title: "PyTorch for Deep Learning: Zero to Mastery",
+    technologies: "Python, PyTorch, Colab",
+    description:
+      "A large course I have started on PyTorch and plan to complete after the TensorFlow Developer Exam",
+    hours: 27,
+    instructor: "Daniel Bourke",
+    courseUrl: "https://academy.zerotomastery.io/courses/1808210",
+    logo: "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://file-uploads.teachablecdn.com/e80aa384d56c4db8b2e6cc9a354af7f1/0c7f883891324637a26ae4f15d1fce7d",
+    courseCertificateUrl: "",
+    isComplete: false,
+  },
+];
